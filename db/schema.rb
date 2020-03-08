f5c062f54bb3f70542f3be169cdbc2d452770f87
hayato sato <hayatosato@hayato-MacBook-Pro.local>
2020-03-08 20:12:24 +0900

    DBをPostgreSQLに変更

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_03_07_141618) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "items", force: :cascade do |t|
     t.string "name"
     t.integer "price"
